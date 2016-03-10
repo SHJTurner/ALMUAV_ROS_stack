720ec54bcac8e2b8542574a624615cd82ad54634
Stig Turner <sttur14@student.sdu.dk>
2016-03-10 11:33:30 +0100

    test

--- monocular_pose_estimator/src/nodelet.cpp
+++ monocular_pose_estimator/src/nodelet.cpp
@@ -12,7 +12,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with RPG-MPE.  If not, see <http://www.gnu.org/licenses/>.
-
+//test
 #include <memory>
 #include <pluginlib/class_list_macros.h>
 
