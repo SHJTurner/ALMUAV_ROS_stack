364dce90f6f43dd9163d7c693fcfa7b433ca2535
Stig Halfdan Juhl Turner <sttur14@student.sdu.dk>
2016-04-21 12:56:21 +0200

    Updated definition names

--- control_node/src/control_node.cpp
+++ control_node/src/control_node.cpp
@@ -10,6 +10,7 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/ExtendedState.h>
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
 #include <std_msgs/Float64.h>
@@ -27,9 +28,11 @@
 ros::ServiceClient set_mode_client;
 ros::Publisher local_pos_pub;
 mavros_msgs::State current_state; //UAV state
+mavros_msgs::ExtendedState extended_current_state; //UAV state
 geometry_msgs::PoseStamped TargetPos;
 double Prev_altitude;
 bool streaming_setpoints;
+bool uav_detected;
 
 //State machine enum
 enum States{
@@ -40,7 +43,9 @@ enum States{
     WAIT_FOR_POS_REACHED, //Wait for the UAV to center it self over the platform
     DESCEND, //Descend until landed
     LANDED, //Landed, Disarm UAV
-    FAILSAFE //Failsafe, land the UAV regardless of position/state
+    DISARMED, //The UAV has landed and is disarmed... dont do anything
+    FAILSAFE, //Failsafe, land the UAV regardless of position/state
+    KILLSWITCH //DISARM THE UAV REGARDLESS OF STATE
 };
 
 //Current_state callback
@@ -48,11 +53,17 @@ void state_cb(const mavros_msgs::State::ConstPtr& msg){
     current_state = *msg;
 }
 
+//Current_state callback
+void extended_state_cb(const mavros_msgs::ExtendedState::ConstPtr& msg){
+    extended_current_state = *msg;
+}
+
 #ifdef SIMULATION
 //Estimated_pose callback (In simulation, the UAVs local pose is used)
 geometry_msgs::PoseStamped estimated_pose;
 void pose_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
         estimated_pose = *msg;
+        uav_detected = true;
 }
 
 #else
@@ -60,6 +71,7 @@ void pose_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
 geometry_msgs::PoseWithCovarianceStamped estimated_pose;
 void pose_cb(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg){
         estimated_pose = *msg;
+        uav_detected = true;
 }
 #endif
 
@@ -67,7 +79,7 @@ void pose_cb(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg){
 void setpoint_center()
 {
 #ifdef SIMULATION
-    if(estimated_pose.pose.position.z < InitPosHeight)
+    if(true)//estimated_pose.pose.position.z < InitPosHeight)
 #else
     if(estimated_pose.pose.pose.position.z < InitPosHeight)
 #endif
@@ -120,16 +132,13 @@ bool enable_offboard()
 {
     mavros_msgs::SetMode offb_set_mode;
     offb_set_mode.request.custom_mode = "OFFBOARD";
-    ROS_INFO("Swithing to OFFBOARD mode");
     if(current_state.mode != "OFFBOARD")
     {
         if( !(set_mode_client.call(offb_set_mode) && offb_set_mode.response.success) )
         {
-            ROS_WARN("Failed to enable OFFBOARD mode!");
             return false;
         }
     }
-    ROS_INFO("OFFBOARD mode enabled");
     return true;
 }
 
@@ -166,17 +175,21 @@ double eu_dist_to_target_point()
 #endif
 }
 
-//non blocking getch function
+//non blocking getch function.. (It blocks!) :/
 int getch()
 {
-  static struct termios oldt, newt;
-  tcgetattr( STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON);
-  tcsetattr( STDIN_FILENO, TCSANOW, &newt);
-  int c = getchar();
-  tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
-  return c;
+    static struct termios oldt, newt;
+    tcgetattr( STDIN_FILENO, &oldt);           // save old settings
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON);                 // disable buffering
+    newt.c_cc[VMIN] = 0;
+    newt.c_cc[VTIME] = 0;
+    tcsetattr( STDIN_FILENO, TCSANOW, &newt);  // apply new settings
+
+    int c = getchar();  // read character (non-blocking)
+
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldt);  // restore old settings
+    return c;
 }
 
 int main(int argc, char **argv)
@@ -185,7 +198,7 @@ int main(int argc, char **argv)
     //----------------------------------------------------------------
     //Init
     //----------------------------------------------------------------
-
+    uav_detected = false;
     ros::init(argc, argv, "control_node");
     ros::NodeHandle nh;
     ros::Rate rate(updateRate);
@@ -201,13 +214,25 @@ int main(int argc, char **argv)
 
     //Subscribe to UAV state
     ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 10, state_cb);
+    //Subscribe to UAV extended state
+    ros::Subscriber extended_state_sub = nh.subscribe<mavros_msgs::ExtendedState>("mavros/extended_state", 10, extended_state_cb);
+
     //Create published for publiching setpoints
     local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local",1); //Queue set to one (Allways publich the newest!)
     //Create service client for setting UAV state
     set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
     //init boolean for streaming commands enabled/disabled
-    streaming_setpoints = false;
+    ros::ServiceClient land_client = nh.serviceClient<mavros_msgs::CommandBool>
+                ("mavros/cmd/land");
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
+                ("mavros/cmd/arming");
 
+
+    mavros_msgs::CommandBool disarm_cmd;
+    disarm_cmd.request.value = false;
+    mavros_msgs::CommandBool land_cmd;
+    land_cmd.request.value = true;
+    streaming_setpoints = false;
     //----------------------------------------------------------------
     //Wait for FCU connection and for the UAV to be armed
     //----------------------------------------------------------------
@@ -239,14 +264,14 @@ int main(int argc, char **argv)
         }
     }
     ROS_INFO("UAV is armed");
-    ROS_INFO("Press \"space\" to trigger FailSafe");
+    ROS_INFO("Press and hold \"space\" to trigger failsafe");
+    ROS_INFO("Press and hold shift + 'D' to disarm immediately");
 
     //----------------------------------------------------------------
     //State machine
     //----------------------------------------------------------------
     int state = IDLE;
     int next_state = IDLE;
-    bool emergency_stop = false;
     bool statePrinted = false;
 
     while(ros::ok())
@@ -256,55 +281,152 @@ int main(int argc, char **argv)
             state = next_state;
             statePrinted = false;
         }
-
-        if(emergency_stop || 32 == getch())
+        int c = getch();
+        if(32 == c)
         {
             state = FAILSAFE;
             next_state = FAILSAFE;
         }
+        if('D' == c)
+        {
+            state = KILLSWITCH;
+            next_state = KILLSWITCH;
+        }
         switch(state)
         {
-            case IDLE:
-                //DONT DO ANYTHING
-                ROS_INFO_ONCE("Idle (press 's' to start system)");
+            case IDLE://Wait for user input
+                if(!statePrinted)
+                {
+                    ROS_INFO("STATE: ILDE");
+                    ROS_INFO_ONCE("Press and hold 's' to start system");
+                    statePrinted = true;
+                }
                 if('s' == getch())
                 {
-                    ROS_INFO("Searching for UAV");
-                    next_state = START_SETPOINT_STREAM;
+                    next_state = SEARCHING;
                 }
                 break;
 
             case SEARCHING:
+                if(!statePrinted)
+                {
+                    ROS_INFO("STATE: SEARCHING");
+                    ROS_INFO("Searching for UAV");
+                    statePrinted = true;
+                }
                 //WAIT FOR UAV TO BE DETECTED
+                if(uav_detected)
+                    next_state = START_SETPOINT_STREAM;
                 break;
 
             case START_SETPOINT_STREAM:
+                if(!statePrinted)
+                {
+                    ROS_INFO("STATE: START_SETPOINT_STREAM");
+                    ROS_INFO("Started setpoint stream");
+                    statePrinted = true;
+                }
                 if(start_setpoint_stream())
                     next_state = ENABLE_OFFBOARD;
                 break;
 
             case ENABLE_OFFBOARD:
+                if(!statePrinted)
+                {
+                    ROS_INFO("STATE: ENABLE_OFFBOARD");
+                    ROS_INFO("Enableling offboard mode");
+                    statePrinted = true;
+                }
                 if(enable_offboard())
+                {
+                    ROS_INFO("Offboard mode enabled");
                     next_state = WAIT_FOR_POS_REACHED;
+                }
+                else
+                    ROS_INFO("Failed to enable offboard mode");
                 break;
 
             case WAIT_FOR_POS_REACHED:
+                if(!statePrinted)
+                {
+                    ROS_INFO("STATE: WAIT_FOR_POS_REACHED");
+                    ROS_INFO("Waiting for UAV to center over platform");
+                    statePrinted = true;
+                }
                 //Go to descend if within 0.05 meters of setpoint
                 if(eu_dist_to_target_point() < 0.30)
+                {
+                    ROS_INFO("UAV centered over platform");
                     next_state = DESCEND;
+                }
                 break;
 
             case DESCEND:
-                if(eu_dist_to_center() < 0.30)
+                if(!statePrinted)
+                {
+                    ROS_INFO("STATE: DESCEND");
+                    ROS_INFO("Staring to descend until landed");
+                    statePrinted = true;
+                }
+                if(extended_current_state.landed_state == mavros_msgs::ExtendedState::LANDED_STATE_ON_GROUND)
+                {
+                    ROS_INFO("Landing detected");
+                    next_state = LANDED;
+                }
+                else if(eu_dist_to_center() < 0.30)
                     descend();
-                //if landed, then disarm
                 else
+                {
+                    ROS_WARN("UAV moved away form center");
+                    ROS_INFO("Retrying landing");
                     next_state = WAIT_FOR_POS_REACHED;
+                }
+                break;
+
+            case LANDED:
+                if(!statePrinted)
+                {
+                    ROS_INFO("STATE: LANDED");
+                    ROS_INFO("UAV has landed");
+                    ROS_INFO("Disarming");
+                    statePrinted = true;
+                }
+                if( arming_client.call(disarm_cmd) &&
+                    disarm_cmd.response.success){
+                    ROS_INFO_ONCE("Disarmed UAV");
+                    next_state = DISARMED;
+                }
+                break;
+
+            case DISARMED:
+                if(!statePrinted)
+                {
+                    ROS_INFO("STATE: DISARMED");
+                    ROS_INFO("Landing compleate");
+                    statePrinted = true;
+                    ROS_INFO("control_node shutting down");
+                    return 0;
+                }
                 break;
 
             case FAILSAFE:
                 //Land
-                ROS_ERROR_ONCE("Failsafe enabled! Landing");
+                ROS_ERROR_ONCE("STOPPED STREAMING SETPOINT! THIS WILL TRIGGER THE FAILSAFE");
+                ROS_ERROR_ONCE("PRESS AND HOLD shift + 'D' TO DISARM UAV NOW!");
+                streaming_setpoints = false;
+                if(extended_current_state.landed_state == mavros_msgs::ExtendedState::LANDED_STATE_ON_GROUND)
+                {
+                    ROS_INFO_ONCE("Landing detected");
+                    next_state = LANDED;
+                }
+                break;
+
+            case KILLSWITCH: //DISARM THE UAV REGARDLESS OF STATE
+                ROS_FATAL_ONCE("KILLSWITCH ENABLED! UAV WILL BE DISARMED IMMEDIATELY!");
+                if( arming_client.call(disarm_cmd) &&
+                    disarm_cmd.response.success){
+                    ROS_FATAL_ONCE("UAV DISARMED!");
+                }
                 break;
         }
 
@@ -321,6 +443,6 @@ int main(int argc, char **argv)
     //----------------------------------------------------------------
     //Shutdown
     //----------------------------------------------------------------
-    ROS_INFO("Shutting down control node");
+    ROS_INFO("control_node shutting down");
     return 0;
 }
